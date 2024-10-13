5974055fd99f78d12fad565da9c2539e770ed66a
Nodirbek <nomonovn2@gmail.com>
2024-10-13 16:16:35 +0500

    changed from CLI to GUI application. added radio checkboxes for http methods with one label

--- main.go
+++ main.go
@@ -1,10 +1,35 @@
-/*
-Copyright © 2024 Nodirbek <nodirbekgolang@gmail.com>
-*/
 package main
 
-import "github.com/rarebek/gemix/cmd"
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+var selectedHttpMethod string
 
 func main() {
-	cmd.Execute()
+	app := app.New()
+	mainWindow := app.NewWindow("gemix - API fuzztester")
+
+	radio := widget.RadioGroup{
+		DisableableWidget: widget.DisableableWidget{},
+		Horizontal:        true,
+		Required:          true,
+		OnChanged: func(selected string) {
+			selectedHttpMethod = selected
+			fmt.Println(selectedHttpMethod)
+		},
+		Options:  []string{"POST", "GET", "PUT", "DELETE"},
+		Selected: "",
+	}
+
+	label := widget.NewLabel("Select HTTP method:")
+
+	mainWindow.SetContent(container.NewVBox(label, &radio))
+	mainWindow.Resize(fyne.NewSize(500, 300))
+	mainWindow.ShowAndRun()
 }
