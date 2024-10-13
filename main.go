b015332911686e56e821819a40b76d30a75f1eab
Nodirbek <nomonovn2@gmail.com>
2024-10-13 16:40:12 +0500

    centered radio and text

--- main.go
+++ main.go
@@ -2,34 +2,33 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
-
-var selectedHttpMethod string
 
 func main() {
 	app := app.New()
 	mainWindow := app.NewWindow("gemix - API fuzztester")
 
-	radio := widget.RadioGroup{
-		DisableableWidget: widget.DisableableWidget{},
-		Horizontal:        true,
-		Required:          true,
-		OnChanged: func(selected string) {
-			selectedHttpMethod = selected
-			fmt.Println(selectedHttpMethod)
-		},
-		Options:  []string{"POST", "GET", "PUT", "DELETE"},
-		Selected: "",
-	}
+	radio := widget.NewRadioGroup([]string{"POST", "GET", "PUT", "DELETE"}, func(selected string) {
+		fmt.Println(selected)
+	})
+	radio.Horizontal = true
+	radio.Required = true
 
-	label := widget.NewLabel("Select HTTP method:")
+	text := canvas.NewText("Choose HTTP method:", color.White)
 
-	mainWindow.SetContent(container.NewVBox(label, &radio))
+	mainContent := container.NewVBox(
+		container.NewCenter(text),
+		container.NewCenter(radio),
+	)
+
+	mainWindow.SetContent(mainContent)
 	mainWindow.Resize(fyne.NewSize(500, 300))
 	mainWindow.ShowAndRun()
 }
